465e60d7701897e548a6c46c63e3c8c0953cd08b
Beyza Betül Yıldız <beyzabetuly@gmail.com>
2023-05-24 15:57:52 +0300

    Psychological test application with C#
    The "Are you fit to take care of a pet" psychological test application written in C#

---
+++ Psikolojik_Test/Psikolojik_Test/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace Psikolojik_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(23, 201);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(462, 92);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 299);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(462, 40);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 345);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(462, 40);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(23, 393);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(462, 40);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(23, 439);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(462, 40);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.SeaGreen;
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button6.Location = new System.Drawing.Point(542, 310);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(155, 102);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "BAŞLA";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(23, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(462, 183);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(521, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 31);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Toplam Puanınız: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(723, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 31);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOrange;
+            this.ClientSize = new System.Drawing.Size(800, 493);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Psikolojik_Test/Psikolojik_Test/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Psikolojik_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int soru;
+        int toplam = 0;
+        private void button6_Click(object sender, EventArgs e)
+        {
+            button6.Text = "Sonraki";
+            soru++;
+
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button6.Enabled = false;
+            if (soru==1)
+            {
+                
+                textBox1.Text = "1-Öncelikle şunu soralım, kediler ve köpekler sahipleri ile bağ kurarlar mı?";
+                button1.Text = "Evet, ancak bunun yalnızca kedi ve köpek ile sınırlandırılmaması gerekir. Tüm canlılar sahipleri ile bağ kurarlar.";
+                button2.Text = "Evet, kedi ve köpeklerin sahibiyle bağ kurduğunu biliyorum.";
+                button3.Text = "Elbette kurarlar.";
+                button4.Text = "Hayır! Hayvanların öyle duyguları yoktur.";
+                
+            }
+            if (soru==2)
+            {
+                textBox1.Text = "2-Evcil hayvanın nasıl bir yaklaşım sergilersin?";
+                button1.Text = "Mutlu";
+                button2.Text = "Dürüst";
+                button3.Text = "Normal davranırım.";
+                button4.Text = "Sinirli";
+            }
+
+
+            if (soru==3)
+            {
+                textBox1.Text = "3-Diyelim ki evin evcil hayvanın için uygun değil. Evini evcil dostun için uygun hâle getirmek için uğraşır mısın? Örneğin kapı ve pencerelere sineklik takmak vb.";
+                button1.Text = "Elbette uygun hâle getiririm.";
+                button2.Text = "Her şeyi yapamayabilirim ama onun da rahat edebileceği bir alan oluştururum.";
+                button3.Text = "Yani bilemiyorum, belki yaparım.";
+                button4.Text = "Neden yapayım ki ? ";
+            }
+
+
+            if (soru==4)
+            {
+
+                textBox1.Text = "4- Kedi ve köpeklere takılan çip hakkında bilgin var mı?";
+                button1.Text = "Evet, onların kaybolmaması ve tehlike anında veterinerlerin bizlere ulaşabilmesi adına oldukça önemlidir.";
+                button2.Text = "Evet, detaylı bilgim yok ama öğrenmeye çalışıyorum.";
+                button3.Text = "Çip ne demek? Hayvanları da çiplemeye mi çalışıyorlar?";
+                button4.Text = "Asla olmaz! Hayvana elektronik bir şey takılmaz!";
+            }
+
+
+            if (soru==5)
+            {
+                textBox1.Text = "5-Hayvanların bir şeyleri kemirmesi, eşyalara zarar vermesi normal midir?";
+                button1.Text = "Tıpkı çocukların bir şeylere zarar vermesi gibi normaldir.";
+                button2.Text = "Neden olmasın? Bizler de bazı şeyleri kırıp dökebiliyoruz.";
+                button3.Text = "Ben asla tahammül edemem!";
+                button4.Text = "Ben asla tahammül edemem!";
+                
+            }
+            
+            if (soru==6)
+            {
+                textBox1.BackColor = Color.PaleTurquoise;
+                label2.BackColor = Color.DarkGray;
+
+                label2.Text = toplam.ToString();
+
+                if (toplam <= 7) 
+                {
+                    textBox1.Text = " SONUÇ:Evcil hayvan bakmaya uygun değilsin. Özgür bir yapıya sahipsin," +
+                        "ancak evcil hayvan bakmak sorumluluk gerektirmektedir.";
+                }
+
+
+                if (toplam<=11 && toplam>=8)
+                {
+                    textBox1.Text = "SONUÇ:Bakıma daha az ihtiyacı olan bir evcil hayvan sahiplenmen daha mantıklı olur.  ";
+
+                }
+
+                if (toplam<=15 &&toplam>=12)
+                {
+                    textBox1.Text = "SONUÇ:Evcil hayvan bakmaya uygunsun.Bu sorumluluğu alabilirsin ancak dikkatli olmalısın.";
+                }
+
+                if (toplam<=20 && toplam>=16)
+                {
+                    textBox1.Text = "SONUÇ:Süper!! Tam bir evcil hayvan insanısın.Seni tebrik ediyoruz.";
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button6.Enabled = true;
+            toplam = toplam + 4;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button6.Enabled = true;
+            toplam = toplam + 3;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button6.Enabled = true;
+            toplam = toplam + 2;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button6.Enabled = true;
+            toplam = toplam + 1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            
+        }
+    }
+}
